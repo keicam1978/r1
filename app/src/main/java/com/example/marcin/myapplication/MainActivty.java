3adf040f2f1e5a921a3d6c9582c6e55194a0a78e
mr <mr@outs.pl>
2018-11-17 17:53:26 +0100

    pierwszy comit

--- app/src/main/java/com/example/marcin/myapplication/MainActivty.java
+++ app/src/main/java/com/example/marcin/myapplication/MainActivty.java
@@ -7,8 +7,8 @@ public class MainActivty extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        fgfgfgd;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activty);
+        gfhdgh;
     }
 }
